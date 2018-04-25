e373a7253bb2cc4a5f5dc1d54b46aa6b46af5d95
Dawod <mhm.dawod1@gmail.com>
2018-04-25 12:26:34 +0200

    some modifications are added

--- src/movitable/Movi.java
+++ src/movitable/Movi.java
@@ -5,6 +5,7 @@
  */
 package movitable;
 
+import java.io.Serializable;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -20,8 +21,10 @@ import java.util.Scanner;
  *
  * @author dawod
  */
-public class Movi {
+public class Movi implements Serializable{
+    private int operation; //it is not colum . it's to detect the request type only
     private String title;
+    
     private List<Date> release = new ArrayList<Date>();
     private List<String> category = new ArrayList<String>();
     private List<String> overview = new ArrayList<String>();
@@ -32,21 +35,37 @@ public class Movi {
     private List<Float> popularity = new ArrayList<Float>();
     private List<Boolean> adult = new ArrayList<Boolean>();
     
-    private Scanner scan = new Scanner(System.in);
-    private String temp;
-    private float tempf;
-    private boolean tempb;
-    private String date;
-    private Date tempd;
+    
+    private String temp = "";
+    private float tempf = 0;
+    private boolean tempb = true;
+    private String date = "";
+    private Date tempd = new Date();
     private DateFormat formatter;
+    private Float ff = new Float(0);
             
-    public Movi(String title){
+    public Movi(String title , int operation){
         this.title=title;
+        this.operation = operation;
+        
+        //
+        this.release.add(new Date());
+        this.category.add("null");
+        this.overview.add("null");
+        this.language.add("null");
+        this.company.add("null");
+        this.country.add("null");
+        this.runtime.add(ff);
+        this.popularity.add(ff);
+        this.adult.add(true);
+        //
+        
     }
     
-
-//--------------------------------------------------------------------------------
+//
+////--------------------------------------------------------------------------------
     public void set_release() throws ParseException{
+        Scanner scan = new Scanner(System.in);
         System.out.println("please type film release date[yyyy-mm-dd]:");
         date = scan.next();
         formatter = new SimpleDateFormat("yyyy-MM-dd");
@@ -57,42 +76,49 @@ public class Movi {
 
 
     public void set_category(){
+        Scanner scan = new Scanner(System.in);
         System.out.println("please type film category:");
         temp = scan.nextLine();
         category.add(temp);
     }
     
     public void set_overview(){
+        Scanner scan = new Scanner(System.in);
         System.out.println("please type film overview:");
         temp = scan.nextLine();
         overview.add(temp);
     }
     
     public void set_language(){
+        Scanner scan = new Scanner(System.in);
         System.out.println("please type film language:");
         temp = scan.nextLine();
         language.add(temp);
     }
     
     public void set_company(){
+        Scanner scan = new Scanner(System.in);
         System.out.println("please type film company:");
         temp = scan.nextLine();
         company.add(temp);
     }
     
     public void set_country(){
+        Scanner scan = new Scanner(System.in);
         System.out.println("please type film country:");
         temp = scan.nextLine();
         country.add(temp);
     }
     
     public void set_runtime(){
+        Scanner scan = new Scanner(System.in);
         System.out.println("please type film runtime:");
         tempf = scan.nextFloat();
         runtime.add(tempf);
     }
 
     public void set_adult(){
+        Scanner scan = new Scanner(System.in);
         System.out.println("is this film for adults?[true or false]");
         tempb = scan.nextBoolean();
         adult.add(tempb);
@@ -100,6 +126,7 @@ public class Movi {
     }
 
     public void set_popularity(){
+        Scanner scan = new Scanner(System.in);
         System.out.println("please type film popularity:");
         tempf = scan.nextFloat();
         popularity.add(tempf);
@@ -144,7 +171,11 @@ public class Movi {
     public List<Float> get_popularity(){
         return this.popularity;
     }
-//-------------------------------------------------------------------------------    
+    
+    public  int get_operation(){
+        return this.operation;
+    }
+////-------------------------------------------------------------------------------    
     
     
     
